c0ae5bd9c3d626ec9137ae15513d96a580c7fdd7
josemarvinlau2009-rgb <josemarvinlau2009@gmail.com>
2026-03-31 07:55:38 +0700

    feat: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -17,7 +17,7 @@
  </header>
     <!-- Main Content -->
     <main class="grow container mx-auto">
-        <div class="mt-8">
+        <div class="mt-8 space-y-4">
             <!--Card Header-->
             <div class="bg-white shadow rounded-lg p-4">
                 <h1 class="text-2xl font-bold ">Daftar Siswa</h1>
@@ -26,7 +26,34 @@
             <!--Card Header End-->
             <!--Card Content-->
             <div class="bg-white shadow rounded-lg p-4 mt-4">
-                
+                <table class="w-full">
+                    <thead class="bg-gray-200">
+                        <tr>
+                            <th class="py-2 px-4 text-left">No</th>
+                            <th class="py-2 px-4 text-left">Nama</th>
+                            <th class="py-2 px-4 text-left">NIS</th>
+                            <th class="py-2 px-4 text-left">Kelas</th>
+                            <th class="py-2 px-4 text-left">No Telepon</th>
+                            <th class="py-2 px-4 ">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="py-2 px-4 text-left">1</td>
+                            <td class="py-2 px-4 text-left">CUMIK</td>
+                            <td class="py-2 px-4 text-left">1234</td>
+                            <td class="py-2 px-4 text-left">11 TKJ 3</td>
+                            <td class="py-2 px-4 text-left">0812345678</td>
+                            <td class="py-2 px-4">
+                                <div class="flex items-center justify-center gap-4">
+                                    <a href="" class="text-green-500">Detail</a>
+                                    <a href="" class="text-yellow-500">Edit</a>
+                                    <a href="" class="text-red-500">Hapus</a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
             <!--Card Content End-->
         </div>
